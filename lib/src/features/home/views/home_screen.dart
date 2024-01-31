5da607453b38db65afa2d30fa81b573b9c607848
Mahdi Mohammed <48333830+mahdi-mk@users.noreply.github.com>
2024-01-31 14:15:07 +0300

    Install GoRouter package

---
+++ lib/src/features/home/views/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: const Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
---
+++ lib/src/router/router.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_app/src/features/home/views/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+enum AppRoute {
+  home,
+}
+
+final router = GoRouter(
+  initialLocation: '/',
+  debugLogDiagnostics: true,
+  routes: [
+    GoRoute(
+      path: '/',
+      name: AppRoute.home.name,
+      builder: (context, state) => const HomeScreen(),
+    ),
+  ],
+);
